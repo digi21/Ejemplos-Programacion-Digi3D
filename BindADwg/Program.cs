de7e7ee1b6b9f2427f1a692fbd91cdd097a0de6d
José Ángel <joseangelmt@outlook.com>
2024-05-16 22:26:15 +0200

    Añadido proyecto BindADwg

---
+++ BindADwg/Program.cs
@@ -0,0 +1,33 @@
+﻿using Digi21.DigiNG.DigiTab;
+using Digi21.DigiNG.IO.BinDouble;
+using Digi21.DigiNG.IO.Dwg;
+
+if (args.Length < 4)
+{
+    Console.Error.WriteLine(
+        "BindADwg [archivo original] [archivo a crear] [tabla de códigos]");
+    return;
+}
+
+
+var rutaArchivoOriginal = args[0];
+var rutaArchivoCrear = args[1];
+var rutaTablaCodigos = args[2];
+
+using var digiTab = DigiTab.Load(rutaTablaCodigos);
+
+using var archivoEntrada = new BinDouble(rutaArchivoOriginal, new DatabaseConnectionProperties(), digiTab);
+
+using var archivoSalida = new Dwg(
+    rutaArchivoCrear,
+    new DwgParameters
+    {
+        Version = DwgVersion.Ac_2010
+    }, 
+    digiTab, () => archivoEntrada.Wkt);
+
+foreach (var geometria in archivoEntrada)
+{
+    archivoSalida.Add(geometria.Clone());
+    Console.Write("+");
+}
