53ffeab9d7d06d51573ea950f2378b3b50f62ea8
José Ángel <joseangelmt@outlook.com>
2024-12-18 11:15:22 +0100

    Añadido proyecto "comprimir"

---
+++ comprimir/Program.cs
@@ -0,0 +1,28 @@
+﻿using Digi21.DigiNG.IO.BinDouble;
+
+if (args.Length < 2)
+{
+    Console.Error.WriteLine("Se esperaban parámetros: [archivo de entrada] [archivo de salida]");
+    return;
+}
+
+if (!File.Exists(args[0]))
+{
+    Console.Error.WriteLine($"El archivo {args[0]} no existe");
+    return;
+}
+
+
+using var entrada = new BinDouble(args[0]);
+using var salida = new BinDouble(args[1], () => entrada.Wkt);
+
+Console.WriteLine($"Comprimiendo archivo {args[0]}...");
+foreach (var geometría in entrada)
+{
+    if (geometría.Deleted)
+        continue;
+
+    salida.Add(geometría.Clone());
+}
+
+Console.WriteLine("Trabajo finalizado");
